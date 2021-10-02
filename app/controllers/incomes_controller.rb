41fadba57ad28ef07d48e42b0092c3473a4fb06c
Hiroaki Ooki <moru150606@gmail.com>
2021-10-02 21:01:32 +0900

    feat:income機能追加

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -16,4 +16,33 @@ class IncomesController < ApplicationController
     @income = Income.find(params[:id])
   end
 
+  def create
+    @income = Income.new(income_params)
+    if @income.save
+      redirect_to @income, notice: '収入科目を登録しました'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @income = Income.find(params[:id])
+    @income.update(income_params)
+    if @income.save
+      redirect_to @income, notice: '収入科目を登録しました'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+    redirect_to :incomes, notice: '科目を削除しました'
+  end
+end
+private
+
+def income_params
+  params.require(:income).permit(:name, :description)
 end
