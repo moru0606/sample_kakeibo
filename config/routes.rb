248c9c6b6c20d81a312f07feb0697739cdaa0f0e
Hiroaki Ooki <moru150606@gmail.com>
2021-10-07 22:00:03 +0900

    feat:income_valueモデル コントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :incomes
   resources :fixdcosts
   resources :variablecosts
+  resources :income_values
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_143059) do
+ActiveRecord::Schema.define(version: 2021_10_07_125201) do
 
   create_table "fixdcosts", force: :cascade do |t|
     t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "income_values", force: :cascade do |t|
+    t.integer "income_id", null: false
+    t.date "year_month"
+    t.integer "value"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
