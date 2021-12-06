5a54fd9ed01ae867675890e17b0a8f23f206b522
moru0606 <moru150606@gmail.com>
2021-12-06 21:34:50 +0900

    feat: fixedcost_values controller

--- app/models/form/income_values.rb
+++ app/models/form/income_values.rb
@@ -1,6 +1,6 @@
 class Form::IncomeValue < IncomeValue
 	REGISTRABLE_ATTRIBUTES = %i(income_id year_month value description)
-	attr_accessor :income_id
+	attr_accessor :fixedcost_id
 	attr_accessor :year_month
 	attr_accessor :value
 	attr_accessor :description
